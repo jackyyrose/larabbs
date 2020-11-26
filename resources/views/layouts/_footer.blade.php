db10bcc3ce9cbc45cfce7c48861366cbab59226d
Jack <jack.xu.nz@gmail.com>
2020-11-26 15:57:45 +1300

    add style to nav and footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="footer">
   <div class="container">
-    <p class="float-left">由 <a href="https://github.com/jackyyrose/larabbs" target="_blank">Jack</a> 设计和编码 <span style="color: #e27575;font-size:
+    <p class="float-left">By <a href="https://github.com/jackyyrose/larabbs" target="_blank">Jack</a> (Design and coding) <span style="color: #e27575;font-size:
     14px;">❤</span>
     </p>
-    <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+    <p class="float-right"><a href="mailto:name@email.com">Contact Us</a></p>
   </div>
 </footer>
