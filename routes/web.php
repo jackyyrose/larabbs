f2f4980e30ad5e72d3ad5389ecd505cb7989d99e
Jack <jack.xu.nz@gmail.com>
2020-11-30 09:54:09 +1300

    data migration and use @guest to show diff nav on home page

--- routes/web.php
+++ routes/web.php
@@ -21,19 +21,19 @@ Route::get('/', [PagesController::class,'root'])->name('root');
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// 用户身份验证相关的路由
+//User Authentication
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-// 用户注册相关路由
+//User Registeration
 Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register']);
-// 密码重置相关路由
+//User Password reset
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
-// Email 认证相关路由
+//User email verification
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
